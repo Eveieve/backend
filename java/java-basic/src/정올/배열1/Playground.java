16ccb652621337e1fc03f2d1cd39eb42fb8fdd75
NJ <jhkdev1@gmail.com>
2025-01-11 18:33:16 +0900

    Commit practice

---
+++ java/java-basic/src/정올/배열1/Playground.java
@@ -0,0 +1,20 @@
+package 정올.배열1;
+
+public class Playground {
+    public static void main(String[] args) {
+        int[] test; // 배열 변수 선언
+        test = new int[5]; // 배열 요소 생성
+
+        test[0] = 80;
+        test[1] = 60;
+        test[2] = 22;
+        test[3] = 50;
+        test[4] = 75;
+
+        for(int i = 0; i<5; i++) {
+            System.out.println((i+1) + "번째 사람의 점수는 " + test[i] + "입니다.") ;
+        }
+    }
+
+
+}
