9d3e7f7b3ea792f681c3452cc3b95a2b006b2a38
zeyi <jhkdev1@gmail.com>
2025-01-13 12:25:40 +0900

    Class notes

--- java/java-basic/src/day04/SignOperatorEx.java
+++ java/java-basic/src/day04/SignOperatorEx.java
@@ -2,28 +2,28 @@ package day04;
 
 public class SignOperatorEx {
     public static void main(String[] args) {
-        //2. 증감 연산자
+        //1. 부호 연산자   + , - 변수: + 연산자는 생략하고 -연산자는 변수값의 부호를 바꿀때 사용
+
 //        byte b = 100;
-//
-//        System.out.println(-b); // 원본 데이터 100은 변경되지 않음
-//        System.out.println((-b)); // 원본 데이터 100은 변경되지 않음
-//
-//        System.out.println(-(-b)); // 원본 데이터 100은 변경되지 않음
+//        System.out.println(-b);
+//        System.out.println(-(-b));
 
-
-        //2. 증감 연산자 (++, --), 단일항 연산자.
-
-        // 변수 단독으로 증강, 감소 연산자 사용할 경우 (앞, 뒤) 결과는 동일하다.
+        //2. 증감연산자 (++,--) 변수의 값을 1 증가시키거나, 1 감소시키는 연산자 (단일항)
+        // 변수 단독으로 증감, 감소 연산자를 사용할 경우 (앞, 뒤) 결과는 동일하다.
 
         int x = 1;
         int y = 1;
+//        int result1 = ++x;
+//        System.out.println(x + " " +result1);
+//        int result2 = x++;    // result2 = x(2) => x = x+1 (3)
+//        System.out.println(x + " " + result2);
+//        //System.out.println(result1  + " " +result2 + " " + x);
 
-        int result1 = ++x; // 선증가 , x = x+1;
-        System.out.println(result1);
-        int result2 = x++; //후증가, 나중에 x를 증가시키기. 현재 x는 2.
-        System.out.println(result2);
-
-        System.out.println(x); // x는 3
+        int result1 = ++x + 10;
+        System.out.println(result1); //12
+        int result2 = y++ + 10;
+        System.out.println(result2);      //11
 
     }
+
 }
--- java/java-basic/src/jungol/Operator/Assess6.java
+++ java/java-basic/src/jungol/Operator/Assess6.java
@@ -1,9 +1,19 @@
 package jungol.Operator;
 
+import java.util.Scanner;
+
 public class Assess6 {
     public static void main(String[] args) {
         // 4가지 관계연산자의 결과 출력하기.
 
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
 
+        boolean bool1 = num1 > num2;
+        int int1 = bool1 ? 1 : 0;
+
+        //boolean bool2 =
+        System.out.printf("%d > %d --- %d", num1, num2, bool1);
     }
 }
