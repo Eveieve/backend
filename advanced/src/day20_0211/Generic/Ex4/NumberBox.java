a724710d22686b2586df75f9e0e258e6dae9d4ea
Eveieve <jhkdev1@gmail.com>
2025-02-11 17:56:32 +0900

    day 20 - notes

--- advanced/src/day20_0211/Generic/Ex4/NumberBox.java
+++ advanced/src/day20_0211/Generic/Ex4/NumberBox.java
@@ -4,7 +4,7 @@ import java.util.List;
 
 //Number 타입만 지원하는 제너릭클래스
 //Number를 확장하는 클래스만 NumberBox의 타입매개변수로 사용할 수 있다.
-public class NumberBox<T extends Number> {
+public class NumberBox<T extends Number> { // 타입 파라미터 티는 넘버 타입을 확장한다.
     private T number;
 
     public void setNumber(T number) {
@@ -13,15 +13,15 @@ public class NumberBox<T extends Number> {
     public double doubleValue(){
         return number.doubleValue();
     }
-    // 상한 제한 와일드카드 (? extends T) 알려지지 않은 유형이 특정 타입 또는 그 하위클래스가 되도록 제한
-    public void printNumberList(List<? extends Number> list){
+    // 상한 제한 와일드카드 (? extends T)
+    public void printNumberList(List<? extends Number> list){ // 알려지지 않은 타입은 넘버 타입이거나 그 하위가 된다 - 확장하고 있기 때문이다.
         for(Number number : list){
             System.out.println(number);
         }
 
     }
     // 어떤 유형과도 함께 사용할 수 있도록 유연성 제공
-    public void printList(List<?> list){
+    public void printList(List<?> list){ // ㅔㅕ
         for(Object element : list) System.out.println(element);
     }
 
