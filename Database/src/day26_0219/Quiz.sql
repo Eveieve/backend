40db323e8f8251d16f3b724d2dd7e07e81fd5556
Eveieve <jhkdev1@gmail.com>
2025-02-19 16:56:42 +0900

    day 26 -quiz

--- Database/src/day26_0219/Quiz.sql
+++ Database/src/day26_0219/Quiz.sql
@@ -40,3 +40,104 @@ SELECT concat(first_name, ' ', last_name) as 'Name', salary, job_id, commission_
 FROM employees
 WHERE commission_pct is not null -- not null 인 사원들만 검색.
 ORDER BY salary desc, commission_pct desc;
+
+
+-----------------------------
+-- 고객별로 주문한 도서의 총 수량과 총 판매액
+SELECT custid as '고객아이디', count(bookid) as 도서수량, sum(saleprice) as 총액
+FROM Orders
+GROUP BY custid;
+
+desc Orders;
+SELECT * FROM Orders;
+
+-- 가격이 8000원 이상인 도서를 구매한 고객에 대하여 고객별 주문 도서의 총 수량 구하기. 단, 두 권이상 구매한 고객만 구하기.
+SELECT custid as 고객아이디, count(bookid) as 도서수량
+FROM Orders
+WHERE saleprice >= 80002
+GROUP BY custid
+HAVING count(bookid) >=2;
+
+select custid, sum(saleprice)
+from orders
+group by custid;
+
+---------------------------------------------------------------------
+-- 도서번호가 1인 도서의 이름
+desc Book;
+SELECT bookname
+FROM Book
+WHERE bookid = 1;
+-- 가격이 2000원 이상인 도서 이름
+desc Book;
+SELECT bookname
+FROM Book
+where price >= 2000;
+-- 박지성의 총 구매액
+desc Customer;
+select * from book;
+select * from orders;
+SELECT * from customer;
+select sum(saleprice)
+from orders
+where custid = 1;
+-- 고객넘버1이 구매한 도서 수
+select * from orders;
+select count(bookid) as '구매도서' -- why bookid?
+from orders
+where custid = 1;
+-- 도서 총 개수
+select * from book;
+select count(bookid) as 도서총개수 -- bookid 하나당 책한개
+from book;
+-- 도서 출고하는 출판사 총 개수
+select * from book;
+select count(publisher) as 출판사총개수
+from book;
+-- 모든 고객 이름, 주소
+select * from customer;
+select address, name from customer;
+-- 2.4
+select * from orders;
+select orderid
+from orders
+where orderdate between '2014-07-04' and '2024-07-04';
+-- 2.5
+select orderid, orderdate
+from orders
+where orderdate not between '2014-07-04' and '2024-07-04';
+-- 2.6
+select name, address
+from customer
+where name like '김%';
+-- 2.7
+select * from customer;
+select name, address
+from customer
+where name like '김%' AND name like '%아';
+
+
+select count(employee_id) as 사원총수
+from employees
+where manager_id IS NOT NULL;
+
+-- 2. 각 사원이 소속된 부서별로 급여 합계, 급여 평균, 급여 최댓값, 급여 최솟값을 집계하고자 한다. 부서번호의 오름차순 정렬하기, 부서에 소속되지 않은 사원에 대한 정보는 제외.
+--
+select * from employees;
+
+select department_id, sum(salary) as 급여합계, avg(salary) as 급여평균, min(salary) as 급여최솟값, max(salary) as 급여최댓값
+from employees
+group by department_id
+having department_id is not null
+order by department_id; -- default is order by asc.
+
+-- 3. 업무별 전체 급여 평균이 > 10000보다 큰 경우 조회하여, 업무, 급여 평균 출력하시오.
+-- 업무에 사원이 포함된 경우 제외 . 전체 급여 평균이 높은 순서대로.
+select * from employees;
+
+select job_id, avg(salary) as 'avg total salary'
+from employees
+group by job_id -- 업무별로 그룹화.. 그룹화 한다는게 뭔지
+having avg(salary) > 10000
+and job_id not like '%CLERK%'
+order by job_id desc;
